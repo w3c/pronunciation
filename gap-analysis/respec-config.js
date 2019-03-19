c2dbc5553c31c2125a972c3ab688efae43a9dca9
ruoxiran <ran@w3.org>
2019-03-19 17:29:13 +0800

    try to fix the spec issue

--- gap-analysis/respec-config.js
+++ gap-analysis/respec-config.js
@@ -20,7 +20,7 @@ var respecConfig = {
 
     // if you wish the publication date to be other than today, set this
     //publishDate:  "2017-05-09",
-    copyrightStart:  "2019",
+    copyrightStart:  "2018",
     license: "w3c-software",
 
     // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
