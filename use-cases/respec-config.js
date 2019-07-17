34c79486eb46c71abbd49cebf169c9c2101d3e94
ruoxiran <ran@w3.org>
2019-07-17 17:38:08 +0800

    transfer use cases

--- use-cases/respec-config.js
+++ use-cases/respec-config.js
@@ -49,12 +49,12 @@ var respecConfig = {
       },
 
       {
-        name: "Michael Cooper",
-        url: 'https://www.w3.org',
-        mailto: "cooper@w3.org",
-        company: "W3C",
-        companyURI: "http://www.w3.org",
-        w3cid: 34017
+        name: "Paul Grenier",
+        url: '',
+        mailto: " ",
+        company: " ",
+        companyURI: "",
+        w3cid: 
       },
       {
         name: "Roy Ran",
