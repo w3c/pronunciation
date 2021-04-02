acdb36ec59bf58dd0b74f938d959af67c1c79245
ruoxiran <ran@w3.org>
2021-04-02 17:57:59 +0800

    added w3c style to sample pages

---
+++ samples/respec-config.js
@@ -0,0 +1,82 @@
+var respecConfig = {
+    // embed RDFa data in the output
+    trace:  true,
+    doRDFa: '1.0',
+    includePermalinks: true,
+    permalinkEdge:     true,
+    permalinkHide:     false,
+    tocIntroductory: true,
+    // specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
+    specStatus:           "unofficial",
+    //noRecTrack: false,
+    //crEnd:                "2012-04-30",
+    //perEnd:               "2013-07-23",
+    //publishDate:          "2013-08-22",
+    //diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
+
+    // the specifications short name, as in http://www.w3.org/TR/short-name/
+    shortName:            "pronunciation-samples",
+    noTOC: true,
+
+
+    // if you wish the publication date to be other than today, set this
+    //publishDate:  "2017-05-09",
+    copyrightStart:  "2021",
+    license: "w3c-software",
+    editors: [
+      
+      {
+        name: "",
+        url: '',
+        mailto: "",
+        company: "",
+        companyURI: "",
+       
+      }
+    ],
+
+    // if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
+    // and its maturity status
+    //previousPublishDate:  "",
+    //previousMaturity:  "",
+    //prevRecURI: "",
+    //previousDiffURI: "",
+
+    // if there a publicly available Editors Draft, this is the link
+    //edDraftURI: "https://w3c.github.io/pronunciation/technical-approach",
+
+    // if this is a LCWD, uncomment and set the end of its review period
+    // lcEnd: "2012-02-21",
+
+    // editors, add as many as you like
+    // only "name" is required
+    
+
+    // authors, add as many as you like.
+    // This is optional, uncomment if you have authors as well as editors.
+    // only "name" is required. Same format as editors.
+
+    //authors:  [
+    //    { name: "Your Name", url: "http://example.org/",
+    //      company: "Your Company", companyURI: "http://example.com/" },
+    //],
+
+    /*
+    alternateFormats: [
+      { uri: 'aria-diff.html', label: "Diff from Previous Recommendation" } ,
+      { uri: 'aria.ps', label: "PostScript version" },
+      { uri: 'aria.pdf', label: "PDF version" }
+    ],
+    */
+
+    // errata: 'http://www.w3.org/2010/02/rdfa/errata.html',
+
+
+
+   // WG info
+   group: 83907
+
+    // Spec URLs
+
+
+  };
